aa85a9c35a33a242ec4204043ec369a8918e28c6
梅丹隆 <meidanlong@maoyan.com>
2021-07-27 11:23:36 +0800

    deploy

--- docs/.vuepress/components/ad.vue
+++ docs/.vuepress/components/ad.vue
@@ -7,7 +7,7 @@
     />
     <p>推荐一个<mark-orange>性能优秀</mark-orange>、<mark-orange>稳定</mark-orange>的小众云服务器-小鸟云</p>
     <p>亲测与其他大型的互联网厂商的产品体验没有什么不同，但是个人学习使用和科学上网<mark-orange>便宜</mark-orange>才是王道！</p>
-    <p>使用我的推荐码：<mark-pink>ZR2GGE</mark-pink> 可以在官网<mark-orange>优惠</mark-orange>的基础上<mark-orange>再打85折</mark-orange></p>
+    <p>使用我的推荐码：<mark-pink>ZR2GGE</mark-pink> 购买云服务器和主机可以在官网<mark-orange>优惠</mark-orange>的基础上<mark-orange>再打85折</mark-orange></p>
     <p>新用户可以点击这个链接直达：<a href="https://www.niaoyun.com/?u=JPHJ68">https://www.niaoyun.com/?u=JPHJ68</a></p>
   </a-card>
 </template>
